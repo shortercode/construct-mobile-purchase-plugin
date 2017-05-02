574b1a2dea6281887c90e954113937977456bce1
Iain Shorter <iainshorter@gmail.com>
2017-05-02 12:23:20 +0100

    added use strict declaration

--- src/js/setKey.js
+++ src/js/setKey.js
@@ -1,6 +1,9 @@
-store.setKey = function (publicKey) {
-	if (store.inappbilling)
-		store.inappbilling.setPublicKey(function () {
-			store.log.debug("Key set: " + publicKey);
-		}, publicKey);
-};
+(function () {
+	'use strict';
+	store.setKey = function (publicKey) {
+		if (store.inappbilling)
+			store.inappbilling.setPublicKey(function () {
+				store.log.debug("Key set: " + publicKey);
+			}, publicKey);
+	};
+}());
